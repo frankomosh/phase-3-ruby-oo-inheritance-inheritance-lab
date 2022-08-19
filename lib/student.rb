a7cd93b09f9d27eef7044c5b01f2a866c42a0000
Frank-197 <frankomosh197@gmail.com>
2022-08-20 01:37:13 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        @knowledge = []
+    end
+    def learn(learnt_something)
+        @knowledge=learnt_something
+    end
 
 end
